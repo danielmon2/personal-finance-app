a2d042ad4ccfcbdc18dc36a6453049158ae60f40
Daniel <dan.nov345@gmail.com>
2026-01-05 18:04:17 +0100

    Make positive amounts of money green

--- src/TransactionsPage/TransactionList/TransactionList.tsx
+++ src/TransactionsPage/TransactionList/TransactionList.tsx
@@ -22,7 +22,12 @@ function TransactionList({ transactionArr }: TransactionListProps) {
             <p className="text--grey">{transactionItem.category}</p>
           </div>
           <div className="margin-left--auto">
-            <p className="text-align--rigth font--bold">
+            <p
+              className={
+                "text-align--rigth font--bold" +
+                (transactionItem.amount > 0 ? " green-text" : "")
+              }
+            >
               {formatMoney(transactionItem.amount)}
             </p>
             <p className="text--grey">{formatDate(transactionItem.date)}</p>
