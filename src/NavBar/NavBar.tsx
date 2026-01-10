573aafda222f9b6f2d59ee450b6c03099879a24f
Daniel <dan.nov345@gmail.com>
2026-01-10 18:42:24 +0100

    Link name in tablet width

--- src/NavBar/NavBar.tsx
+++ src/NavBar/NavBar.tsx
@@ -16,20 +16,30 @@ function NavBar() {
 
   return (
     <nav className="navbar navbar--primary">
-      {paths.map((path) => {
+      {paths.map((path, index) => {
         let activeIcon = "";
         let activeClass = "";
+        let linkName = " navbar__link__name--primary";
         if (URL === "/" + path) {
           activeIcon = "-active";
           activeClass = " navbar__link--active";
+          if (!isMobileWidth) {
+            linkName = " navbar__link__name--active";
+          }
         }
         return (
           <Link
             className={"navbar__link" + activeClass}
             to={"/" + path}
             onClick={() => setURL("/" + path)}
+            key={index}
           >
             <img src={`./images/icon-nav-${path}${activeIcon}.svg`} />
+            {!isMobileWidth && (
+              <p className={"navbar__link__name font--bold" + linkName}>
+                {path[0].toUpperCase() + path.slice(1)}
+              </p>
+            )}
           </Link>
         );
       })}
