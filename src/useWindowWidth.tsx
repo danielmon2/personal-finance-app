e884e7cfdcafaa2695542925127b11ad4fb9211d
Daniel <dan.nov345@gmail.com>
2026-01-14 13:53:02 +0100

    Custom window width hook

---
+++ src/useWindowWidth.tsx
@@ -0,0 +1,37 @@
+import { useSyncExternalStore } from "react";
+
+let lastWindowWidth = {
+  mobile: false,
+  tablet: false,
+  desktop: false,
+};
+
+export function useWindowWidth() {
+  return useSyncExternalStore(subscribe, getSnapshot);
+}
+
+function getSnapshot() {
+  const newWindowWidth = {
+    mobile: window.innerWidth <= 767,
+    tablet: window.innerWidth <= 1280,
+    desktop: window.innerWidth > 1280,
+  };
+
+  for (const [newKey, newEntry] of Object.entries(newWindowWidth)) {
+    for (const [oldKey, oldEntry] of Object.entries(lastWindowWidth)) {
+      if (newKey === oldKey) {
+        if (newEntry !== oldEntry) {
+          lastWindowWidth = newWindowWidth;
+          return newWindowWidth;
+        }
+      }
+    }
+  }
+
+  return lastWindowWidth;
+}
+
+function subscribe(callback: () => void) {
+  window.addEventListener("resize", callback);
+  return () => window.removeEventListener("resize", callback);
+}
