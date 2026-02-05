224843f1ae0dd6c146b16e8df6a4afd538c43102
Daniel <dan.nov345@gmail.com>
2026-02-05 12:03:40 +0100

    Accept all button props in FloatingSelectOption

--- src/util/FloatingSelect/FloatingSelectOption/FloatingSelectOption.tsx
+++ src/util/FloatingSelect/FloatingSelectOption/FloatingSelectOption.tsx
@@ -1,8 +1,9 @@
+import React from "react";
 import type { JSX } from "react";
 
 import "./FloatingSelectOption.css";
 
-type FloatingSelectOptionProps = {
+type FloatingSelectOptionProps = React.ComponentPropsWithoutRef<"button"> & {
   children: JSX.Element | string;
   isActive?: boolean;
   handleClick: (...args: unknown[]) => void;
@@ -12,11 +13,13 @@ function FloatingSelectOption({
   children,
   isActive,
   handleClick,
+  ...rest
 }: FloatingSelectOptionProps) {
   return (
     <button
       onClick={handleClick}
       className={`floating-select__option floating-select__option--${isActive ? "active" : "primary"}`}
+      {...rest}
     >
       {children}
     </button>
